6115e284877945b0c706c17715c6cb13b6c170e1
Ahmet <leaforewer.net@gmail.com>
2021-09-05 19:46:39 +0200

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Events</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
@@ -26,5 +26,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4  {
+  font-size: 20px;
 }
 </style>
